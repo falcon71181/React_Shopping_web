75c91d45b7ab935736dfc766b599adbffb6d7616
falcon71181 <dertyabbuiloveyou71@gmail.com>
2023-10-06 09:34:02 -0400

    update

--- src/components/lists.js
+++ src/components/lists.js
@@ -48,6 +48,55 @@ const computerParts = [
     cost: 99.99,
     qty: 0
   },
+  {
+    id: "item_1001",
+    name: "Intel Core i9 CPU",
+    src: "https://upload.wikimedia.org/wikipedia/commons/0/06/Intel_Core_i9_Logo_2020.png",
+    cost: 350,
+    qty: 0
+  },
+  {
+    id: "item_1002",
+    name: "ASUS ROG Strix NVIDIA GeForce RTX 3080",
+    src: "https://m.media-amazon.com/images/I/81SzNmM27EL._AC_SL1500_.jpg",
+    cost: 799,
+    qty: 0
+  },
+  {
+    id: "item_1003",
+    name: "Corsair Vengeance RGB Pro 32GB (2x16GB) DDR4 3600",
+    src: "https://m.media-amazon.com/images/I/61GpY38PAWL._AC_SL1200_.jpg",
+    cost: 150,
+    qty: 0
+  },
+  {
+    id: "item_1004",
+    name: "AMD Ryzen 9 5950X 16-core, 32-thread",
+    src: "https://m.media-amazon.com/images/I/616VM20+AzL._AC_SL1384_.jpg",
+    cost: 380,
+    qty: 0
+  },
+  {
+    id: "item_1005",
+    name: "MSI Gaming GeForce RTX 3060 12GB 15 Gbps GDRR6 192-Bit",
+    src: "https://m.media-amazon.com/images/I/71tduSp8ooL._AC_SL1500_.jpg",
+    cost: 299,
+    qty: 0
+  },
+  {
+    id: "item_1006",
+    name: "TEAMGROUP T-Force Delta RGB DDR5 Ram 32GB (2x16GB) 6000MHz",
+    src: "https://m.media-amazon.com/images/I/81ov4cFmdaL._AC_SL1500_.jpg",
+    cost: 120,
+    qty: 0
+  },
+  {
+    id: "item_1007",
+    name: "Crucial BX500 2TB 3D NAND SATA 2.5-Inch Internal SSD",
+    src: "https://m.media-amazon.com/images/I/61GSLwetjnL._AC_SL1500_.jpg",
+    cost: 99.99,
+    qty: 0
+  },
   
 ];
 
